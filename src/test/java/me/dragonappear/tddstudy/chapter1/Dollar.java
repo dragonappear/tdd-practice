496cc1ff4862ebafc8c023ace593fd7b8e17d7c1
dragonappear <dragonappear1@gmail.com>
2022-11-12 21:48:26 +0900

    feat : Fix Test
    1. equals() 테스트 해결

--- src/test/java/me/dragonappear/tddstudy/chapter1/Dollar.java
+++ src/test/java/me/dragonappear/tddstudy/chapter1/Dollar.java
@@ -11,4 +11,15 @@ public class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Dollar && ((Dollar) obj).amount == this.amount) {
+            return true;
+        }
+
+        return false;
+
+
+    }
 }
--- src/test/java/me/dragonappear/tddstudy/chapter1/test.java
+++ src/test/java/me/dragonappear/tddstudy/chapter1/test.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class test {
 
     @DisplayName("곱셈")
@@ -11,10 +14,15 @@ public class test {
     void 곱셈(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        Assertions.assertThat(10).isEqualTo(product.amount);
+        assertThat(10).isEqualTo(product.amount);
 
         product = five.times(3);
-        Assertions.assertThat(15).isEqualTo(product.amount);
+        assertThat(15).isEqualTo(product.amount);
+    }
 
+    @Test
+    void testEquality() {
+        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
+        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
 }
