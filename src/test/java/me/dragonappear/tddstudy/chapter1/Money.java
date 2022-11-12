834e150ccb85c1c007c33c3d9c5626fd4c61a9ca
dragonappear <dragonappear1@gmail.com>
2022-11-12 22:27:20 +0900

    feat : Fix Test
    - 동일한 메서드(times)의 두 변이형 메서드 서명부를 통일시킴으 중복제거를 향한 한 단계 전진
    - 팩토리 메서드를 도입하여 테스크 코드에서 콘크리트 하위 클래스의 존재 사실을 분리
    - 하위 클래스가 사라지면 몇몇 테스트는 불필요한 여분의 것이 된다는 것을 인식했다. 하지만 일단 그냥 뒀다.

--- src/test/java/me/dragonappear/tddstudy/chapter1/Money.java
+++ src/test/java/me/dragonappear/tddstudy/chapter1/Money.java
@@ -1,10 +1,21 @@
 package me.dragonappear.tddstudy.chapter1;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
+
 }
--- src/test/java/me/dragonappear/tddstudy/chapter1/Test1.java
+++ src/test/java/me/dragonappear/tddstudy/chapter1/Test1.java
@@ -8,18 +8,18 @@ public class Test1 {
 
     @Test
     void testDollarMultiplication(){
-        Dollar five = new Dollar(5);
-        assertThat(new Dollar(10)).isEqualTo(five.times(2));
-        assertThat(new Dollar(15)).isEqualTo(five.times(3));
+        Money five = Money.dollar(5);
+        assertThat(Money.dollar(10)).isEqualTo(five.times(2));
+        assertThat(Money.dollar(15)).isEqualTo(five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
-        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
-        assertThat(new Franc(5)).isEqualTo(new Franc(5));
-        assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
-        assertThat(new Franc(5)).isNotEqualTo(new Dollar(5));
+        assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
+        assertThat(Money.dollar(5)).isNotEqualTo(Money.dollar(6));
+        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
+        assertThat(Money.franc(5)).isNotEqualTo(Money.franc(6));
+        assertThat(Money.franc(5)).isNotEqualTo(Money.dollar(5));
     }
 
 }
